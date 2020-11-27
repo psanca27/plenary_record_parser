c06af3919d2da51ca5ad4d587f14c54412ffc73c
Pietro Sancassani <sancassani@ifo.de>
2020-11-27 18:43:05 +0100

    added strip method

--- code/create_df_bw.py
+++ code/create_df_bw.py
@@ -6,7 +6,7 @@ df = df[['date', 'executive', 'ministerium', 'party', 'president', 'seq', 'serva
 df['deputy'] = df['role']=='mp'
 df.rename(columns={'executive': 'minister'}, inplace=True)
 df.loc[df['role'] == 'secretary','minister'] = True
-df['speech'] = df[['date', 'seq', 'speaker', 'speech']].groupby(['date', 'seq', 'speaker'])['speech'].transform(lambda x: ' '.join(x))
+df['speech'] = df[['date', 'seq', 'speaker', 'speech']].groupby(['date', 'seq', 'speaker'])['speech'].transform(lambda x: ' '.join(x).strip())
 df = df.groupby(['date', 'speaker', 'seq'], as_index=False).first()
 df = df.set_index(['date', 'seq']).sort_index().reset_index()
 df['speaker'] = df['speaker'].apply(lambda x: x.replace(': ', '').replace('<poi_end>', '').strip())
