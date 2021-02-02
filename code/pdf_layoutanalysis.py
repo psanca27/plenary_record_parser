de6040666aa3765e3c95031598c2d1d7fba3c2d8
Pietro Sancassani <sancassani@ifo.de>
2021-02-02 19:19:55 +0100

    added codes to print most frequent values

--- code/pdf_layoutanalysis.py
+++ code/pdf_layoutanalysis.py
@@ -3,9 +3,11 @@ import os
 import json
 from collections import Counter
 from tqdm import tqdm
+import pandas as pd
 
 
-DATA_PATH = r'../data/SN/'
+
+DATA_PATH = r'../data/TH/'
 
 
 with open(os.path.join(DATA_PATH, "c_x0.json"), encoding="utf-8") as fp:
@@ -35,3 +37,18 @@ with open(os.path.join(DATA_PATH, "y1_occurences_ls.json"), encoding="utf-8") as
 [text_boxes[z] for z in [i for i,x in enumerate(x0_occurences_ls) if x==304]]
 
 [j for j in [int(i) for i in c_x0.keys()] if (j<311) and (j>250)]
+
+
+df_cx0 = pd.DataFrame.from_dict(c_x0, orient='index')
+df_cx0.index = df_cx0.index.astype(int)
+print('cx0:', df_cx0.sort_values(by = 0, ascending=False).head(20))
+print('\n')
+
+df_cy0 = pd.DataFrame.from_dict(c_y0, orient='index')
+df_cy0.index = df_cy0.index.astype(float)
+print('cy0:', df_cy0.sort_values(by = 0, ascending=False).head(20))
+print('\n')
+
+df_cy1 = pd.DataFrame.from_dict(c_y1, orient='index')
+df_cy1.index = df_cy1.index.astype(float)
+print('cx0:', df_cy1.sort_values(by = 0, ascending=False).head(20))
